9d2c9613d15bf68054f377a1a4ef16643dea364b
Gustavo Baron <gustavoalecxander9@gmail.com>
2025-08-25 14:56:17 -0300

    Update ConnectionFactory.java

--- dao/src/main/java/br/com/imobiliaria_db/database/ConnectionFactory.java
+++ dao/src/main/java/br/com/imobiliaria_db/database/ConnectionFactory.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-    private static final String URL = "jdbc:mysql://localhost:3306/ImobDataBase";
+    private static final String URL = "jdbc:mysql://localhost:3306/imobiliaria.db";
     private static final String USER = "root";
-    private static final String PASSWORD = "senha20013"; // ajuste se necessário
+    private static final String PASSWORD = "univille"; // ajuste se necessário
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
